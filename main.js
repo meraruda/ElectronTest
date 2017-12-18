3e56ede8ef9143ff519831475c3d71cbc570a1e1
Wilson WS Huang/WHQ/Wistron <Wilson_WS_Huang@wistron.com>
2017-12-18 11:33:56 +0800

    init

--- main.js
+++ main.js
@@ -56,5 +56,7 @@ app.on('activate', function () {
   }
 })
 
+//app.setAccessiblitySupportEnabled(enabled)
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
